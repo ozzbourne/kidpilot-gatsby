4a8b555b677a6851f21396225f66284a1c35af3e
Oskar Brandt <ogrilus@Oskars-MacBook-Pro.local>
2019-04-25 15:57:20 +0200

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,82 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Header from '../components/header'
+import ogImage from '../images/metaImg-index.jpg'
+import { Link } from 'gatsby'
+import Helmet from 'react-helmet'
+import $ from 'jquery'
 
-const NotFoundPage = () => (
-  <Layout>
-    <Header />
-    <div className="page404">
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
-)
+class NotFoundPage extends React.Component {
+  componentDidMount() {
+    setTimeout(function() {
+      $('h1.hidden, img.hero-image').removeClass('hidden')
+    }, 300)
+
+    setTimeout(function() {
+      $(
+        'h5.hidden, h2.hidden, h6.hidden, h4.hidden, .apple-link.hidden'
+      ).removeClass('hidden')
+    }, 500)
+  }
+  render() {
+    return (
+      <Layout>
+        <Helmet
+          title={'Kidpilot - We make apps you love to use'}
+          meta={[
+            {
+              name: 'description',
+              content:
+                'We create tools for mac, web and mobile that help you focus on what you do best and generally make your life easier.',
+            },
+            {
+              property: 'og:title',
+              content: 'Kidpilot - We make apps you love to use',
+            },
+            { property: 'og:url', content: 'https://wearekidpilot.com/' },
+            {
+              property: 'og:description',
+              content:
+                'We create tools for mac, web and mobile that help you focus on what you do best and generally make your life easier.',
+            },
+            {
+              property: 'og:image',
+              content: `https://wearekidpilot.com${ogImage}`,
+            },
+            { property: 'og:image:width', content: `1200` },
+            { property: 'og:image:height', content: `628` },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <div className="notFoundPage">
+          <Header />
+          <div className="section jumbotron-custom">
+            <div className="hero-bottom">
+              <div className="hero-wrapper">
+                <div className="text-wrapper container">
+                  <h1 className="hidden">404</h1>
+                  <h5 className="hidden">
+                    Sorry, we can´t find what you are looking for.
+                  </h5>
+                  <h5 className="hidden">
+                    <div className="row">
+                      <Link to="/">
+                        <p className="button">Return Home</p>
+                      </Link>
+                    </div>
+                  </h5>
+                </div>
+              </div>
+              <div className="cta">
+                <div className="moon" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default NotFoundPage
