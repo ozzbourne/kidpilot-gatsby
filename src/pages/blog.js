1b81646d405413f3c02024bcd807ce074bcc585d
Oskar <oskar@fatbrothers.se>
2019-05-31 14:38:22 +0200

    Blog page test iframe

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,39 +1,40 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Header from '../components/header'
+import Helmet from 'react-helmet'
 
 class NotFoundPage extends React.Component {
   render() {
     return (
       <Layout>
-        {/* <Helmet
-          title={'Kidpilot - We make apps you love to use'}
-          meta={[
-            {
-              name: 'description',
-              content:
-                'We create tools for mac, web and mobile that help you focus on what you do best and generally make your life easier.',
-            },
-            {
-              property: 'og:title',
-              content: 'Kidpilot - We make apps you love to use',
-            },
-            { property: 'og:url', content: 'https://wearekidpilot.com/' },
-            {
-              property: 'og:description',
-              content:
-                'We create tools for mac, web and mobile that help you focus on what you do best and generally make your life easier.',
-            },
-            {
-              property: 'og:image',
-              content: `https://wearekidpilot.com${ogImage}`,
-            },
-            { property: 'og:image:width', content: `1200` },
-            { property: 'og:image:height', content: `628` },
-          ]}
+        <Helmet
+          title={'Kidpilot - Lastest news'}
+          // meta={[
+          //   {
+          //     name: 'description',
+          //     content:
+          //       'We create tools for mac, web and mobile that help you focus on what you do best and generally make your life easier.',
+          //   },
+          //   {
+          //     property: 'og:title',
+          //     content: 'Kidpilot - We make apps you love to use',
+          //   },
+          //   { property: 'og:url', content: 'https://wearekidpilot.com/' },
+          //   {
+          //     property: 'og:description',
+          //     content:
+          //       'We create tools for mac, web and mobile that help you focus on what you do best and generally make your life easier.',
+          //   },
+          //   {
+          //     property: 'og:image',
+          //     content: `https://wearekidpilot.com${ogImage}`,
+          //   },
+          //   { property: 'og:image:width', content: `1200` },
+          //   { property: 'og:image:height', content: `628` },
+          // ]}
         >
           <html lang="en" />
-        </Helmet> */}
+        </Helmet>
         <div className="blogPage">
           <Header />
           <iframe
@@ -41,6 +42,7 @@ class NotFoundPage extends React.Component {
             width="100%"
             frameborder="0"
             src="https://blog.wearekidpilot.com"
+            title="Blog"
           />
         </div>
       </Layout>
