99d2c281b70cfffe2976c624598d7c518192ff44
Oskar <oskar@fatbrothers.se>
2019-06-01 11:52:25 +0200

    Test analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,13 @@ module.exports = {
         icon: 'src/images/logohead@2x.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-46067561-1',
+        head: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
